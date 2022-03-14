1baeb218f8ab0d620b0a2f1cfa282367246bef3e
ephe <ephe@student42.fr>
2022-03-14 11:08:52 +0100

    feat-ex04-prediction : done

---
+++ ex04/prediction.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def predict_(x, theta):
+    """Computes the vector of prediction y_hat from two non-empty numpy.array.
+    Args:
+    x: has to be an numpy.array, a vector of shape m * 1.
+    theta: has to be an numpy.array, a vector of shape 2 * 1.
+    Returns:
+    y_hat as a numpy.array, a vector of shape m * 1.
+    None if x or theta are empty numpy.array.
+    None if x or theta shapes are not appropriate.
+    None if x or theta is not of the expected type.
+    Raises:
+    This function should not raise any Exception.
+    """
+    if (
+        isinstance(x, np.ndarray)
+        and x.size != 0
+        and x.shape[1] == 1
+        and isinstance(theta, np.ndarray)
+        and theta.size != 0
+        and theta.shape == (2, 1)
+    ):
+        x = np.insert(x, 0, values=1.0, axis=1).astype(float)
+        return x @ theta
+    return
+
+
+if __name__ == "__main__":
+    x = np.arange(1, 6).reshape(-1, 1)
+
+    theta1 = np.array([[5], [0]])
+    print(predict_(x, theta1))
+    # Output: array([[5.],[5.],[5.],[5.],[5.]])
+
+    theta2 = np.array([[0], [1]])
+    print(predict_(x, theta2))
+    # Output: array([[1.],[2.],[3.],[4.],[5.]])
+
+    theta3 = np.array([[5], [3]])
+    print(predict_(x, theta3))
+    # Output: array([[ 8.],[11.],[14.],[17.],[20.]])
+
+    theta4 = np.array([[-3], [1]])
+    print(predict_(x, theta4))
+    # Output: array([[-2.],[-1.],[ 0.],[ 1.],[ 2.]])
